a704037c1c50208d92a8dd8cea2c13986a2ae46a
eron97 <eronsantosbetine@gmail.com>
2023-06-07 10:39:05 -0300

    update main.go

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ Aqui você iniciará o servidor HTTP e configurará as dependências necessária
 
 */
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/eron97/api_go/routes"
+)
+
+func main() {
+	// Configuração das rotas
+	router := routes.SetupRouter()
+
+	// Iniciando o servidor HTTP
+	port := ":8080" // Porta em que o servidor será executado
+	fmt.Printf("Servidor rodando em http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
 }
