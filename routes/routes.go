d7a613fea3dbeb2e21e4cb873e3f8941783e2b63
eron97 <eronsantosbetine@gmail.com>
2023-06-07 10:28:55 -0300

    routers update

--- routes/routes.go
+++ routes/routes.go
@@ -6,3 +6,23 @@ Esta pasta contém as definições das rotas da sua API.
 Aqui você pode mapear os endpoints (URLs) da sua API para os handlers correspondentes.
 
 */
+
+import (
+	"net/http"
+
+	"github.com/eron97/api_go/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupRouter() *mux.Router {
+	router := mux.NewRouter()
+
+	// Rota para a página de login
+	router.HandleFunc("/", handlers.HomeHandler).Methods(http.MethodGet)
+
+	// Rota para o dashboard do perfil
+	router.HandleFunc("/dashboard", handlers.DashboardHandler).Methods(http.MethodGet)
+
+	return router
+}
